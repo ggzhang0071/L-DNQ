1bee096e1caa82308e78ecaf3f11ea0065406739
victor <vili@nvidia.com>
2019-10-24 11:28:02 +0800

    add jupyter

--- victor/docker_cmd.sh
+++ victor/docker_cmd.sh
@@ -2,8 +2,8 @@ img="nvcr.io/nvidia/tensorflow:19.01-py3"
 TOP=`pwd`/..
 
 nvidia-docker run --privileged=true  -e DISPLAY  --net=host --ipc=host -d --rm  -p 7022:22 -p 5022:5022 \
-     -v $TOP:/wrk \
-     -w /wrk  \
+      -v $TOP:/git \
+      -p 6033:8888 \
      $img  tail -f /dev/null
 
 
---
+++ victor/start_jupyter.sh
@@ -0,0 +1,5 @@
+pip install torch-geometric
+jupyter notebook --generate-config
+jupyter notebook password
+jupyter-lab --allow-root --ip 0.0.0.0
+
