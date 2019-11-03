35685808b60e854cbec090dbca4d8a246d31aa3d
ggzhang <155051843@qq.com>
2019-11-03 15:40:51 +0000

    Batchsi

--- victor/run_ContractionCoefficientsChanges.sh
+++ victor/run_ContractionCoefficientsChanges.sh
@@ -5,17 +5,16 @@ name='L-DNQ_contraction_coefficients'
 cd $TOP
 dataset='CIFAR10'
 
-<<'COMMENT'
 for i in 0.1 0.2 0.4 0.6 
 do
  echo "Contraction coefficients $i"
- python3  train_base_model.py --ConCoeff $i --Epochs 10 2>&1 |tee Logs/${name}_${dataset}_$timestamp.log
+ python3 -m pdb  train_base_model.py --ConCoeff $i --Epochs 10 --MentSize 10 2>&1 |tee Logs/${name}_${dataset}_$timestamp.log
 done
-COMMENT
 
-#  
-for i in 64 128 256 512 1024
+<<'COMMENT'
+for i in 64 128 256 512 1024 
 do
  echo "Batch size $i"
- python3  train_base_model.py --BatchSize $i --Epochs 80 --MentSize 20 2>&1 |tee Logs/${name}_${dataset}_$timestamp.log
+ python3  train_base_model.py --BatchSize $i --Epochs 80 --MentSize 10 2>&1 |tee Logs/${name}_${dataset}_$timestamp.log
 done
+COMMENT
