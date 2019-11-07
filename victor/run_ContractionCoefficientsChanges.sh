7ea0da2e5190699e1543704abb0164aac0bd319d
gwli <gwli@outlook.com>
2019-11-07 00:51:00 +0000

    tmp

--- victor/run_ContractionCoefficientsChanges.sh
+++ victor/run_ContractionCoefficientsChanges.sh
@@ -1,24 +1,25 @@
 TOP=`pwd`/..
 timestamp=`date +%Y%m%d%H%M%S`
 #cl=`git rev-parse HEAD|cut -c1-7`
-name='L-DNQ_Contraction_Coefficients'
 cd $TOP
 #  'CIFAR10'
 dataset='MNIST' 
 
+<<COMMENT
+name='L-DNQ_Batch_Size'
 #
+for i in 128 256 512 1024 
+do
+ echo "Batch size $i"
+ python3  train_base_model.py --dataset $dataset --BatchSize $i --Epochs 80 --MentSize 10 2>&1 |tee Logs/${name}_${dataset}_$i_$timestamp.log
+done
+COMMENT
+
+name='L-DNQ_Contraction_Coefficients'
+
 for i in 0.1 0.2 0.4 0.6 
 do
  echo "Contraction coefficients $i"
- python3 train_base_model.py --dataset $dataset --BatchSize 64 --ConCoeff $i --Epochs 80 --MentSize 5 2>&1 |tee Logs/${name}_${dataset}_$timestamp.log
+ python3 train_base_model.py --dataset $dataset --BatchSize 512 --ConCoeff $i --Epochs 80 --MentSize 10 2>&1 |tee Logs/${name}_${dataset}_$i_$timestamp.log
 done
  
-
-
-name='L-DNQ_Batch_Size'
-#
-for i in 64 128 256 512 1024 
-do
- echo "Batch size $i"
- python3  train_base_model.py --dataset $dataset --BatchSize $i --Epochs 80 --MentSize 20 2>&1 |tee Logs/${name}_${dataset}_$timestamp.log
-done
