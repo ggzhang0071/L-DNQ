4608926440740a700b1c45d3bb9431f2b167fa56
ggzhang <155051843@qq.com>
2019-11-10 15:41:55 +0000

    modify the contraction coefficients

--- victor/run_ContractionCoefficientsChanges_2gpus.sh
+++ victor/run_ContractionCoefficientsChanges_2gpus.sh
@@ -9,7 +9,7 @@ rm -rf ../Logs/
 
 dataset='MNIST' 
 
-name='L-DNQ_Batch_Size'
+name='LDNQ_Batch_Size'
 <<COMMENT
 #
 for i in 128 256 512 1024 
@@ -19,23 +19,23 @@ do
 done
 COMMENT
  
-name='L-DNQ_Contraction_Coefficients'
+name='LDNQ_Contraction_Coefficients'
 
 
-<<COMMENT
-for i in 0.1 0.2 0.4 0.6 0.8
+for i in  0.1 0.2
 do
  echo "Contraction coefficients $i"
  python3  train_base_model.py --dataset $dataset --gpus '2,3' --BatchSize 512 --ConCoeff $i --Epochs 80 --MonteSize 10 --print_device_useage 'True' --resume 'True' --return_output 'False' 2>&1 |tee Logs/${name}_${dataset}_${i}_$timestamp.log
 done
-COMMENT
 
 
+
+<<COMMENT
 
 for i in 0.1 0.2 0.4 0.6 0.8
 do
  echo "Contraction coefficients $i"
  python3   train_base_model.py --dataset $dataset --gpus '2,3' --BatchSize 512 --ConCoeff $i --Epochs 4 --MonteSize 2 --print_device_useage 'False' --resume 'True' --return_output 'False' 2>&1 |tee Logs/${name}_${dataset}_${i}_$timestamp.log
 done
-
+COMMENT
 
--- victor/run_ContractionCoefficientsChanges_8gpu.sh
+++ victor/run_ContractionCoefficientsChanges_8gpu.sh
@@ -5,7 +5,7 @@ name='L-DNQ_Contraction_Coefficients'
 cd $TOP
 #
 dataset='CIFAR10' 
-for i in 0.1 0.2 0.4 0.6 0.8
+for i in  0.1  0.2 
 do
  echo "Contraction coefficients $i"
  python3 train_base_model.py --gpus "0,1,2,3,4,5,6,7" --dataset $dataset --BatchSize 512 --ConCoeff $i --Epochs 80 --MonteSize 20  --print_device_useage 'True' --resume 'True' --return_output 'False' 2>&1 |tee Logs/${name}_${dataset}_${i}_$(date +%y%m%d%H%M%S).log
